e7c07c9090b5baeca0eb987aaea9749b8d39a351
Naveen991222 <103424880+Naveen991222@users.noreply.github.com>
2024-09-12 14:02:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
 
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+}
