818ae0eff9a8d945c41dc56b6a89caeb6c98a170
Naveen991222 <103424880+Naveen991222@users.noreply.github.com>
2024-09-12 16:29:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,53 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
-resource "azurerm_resource_group" "aks_rg" {
+variable "resource_group_name" {
+  description = "The name of the Azure resource group"
+  type        = string
+  default     = "my-resource-group"
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+  default     = "my-aks-cluster"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the AKS cluster"
+  type        = number
+  default     = 1
+}
+
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
+resource "azurerm_kubernetes_cluster" "example" {
   name                = var.aks_cluster_name
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = var.aks_cluster_name
+
   default_node_pool {
     name       = "default"
     node_count = var.node_count
     vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Testing"
   }
 }
