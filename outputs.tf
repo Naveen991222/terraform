8652863cf91b0a41e48a4d3f58f2df83061f2297
Naveen991222 <103424880+Naveen991222@users.noreply.github.com>
2024-09-13 17:15:46 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,13 @@
-# Output the AKS cluster kube config file
 output "kube_config" {
-  description = "Kube config for AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-}
-
-# Output resource group name
-output "resource_group_name" {
-  value = azurerm_resource_group.aks_rg.name
-}
-
-# Output AKS cluster name
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks_cluster.name
+  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
   sensitive = true
+}
+
+output "aks_cluster_name" {
+  value     = azurerm_kubernetes_cluster.aks_cluster.name
+  sensitive = true
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
 }
