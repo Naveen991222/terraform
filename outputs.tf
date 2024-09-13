37eda82e4f551deb31ab817601915c8b4435ce81
Naveen991222 <103424880+Naveen991222@users.noreply.github.com>
2024-09-13 17:20:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,17 @@
+# Output the AKS cluster kube config file
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-  sensitive = true
+  description = "Kube config for AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  sensitive   = true  # Marking this as sensitive since it's sensitive data
 }
 
-output "aks_cluster_name" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.name
-  sensitive = true
-}
-
+# Output resource group name
 output "resource_group_name" {
-  value = azurerm_resource_group.main.name
+  value = azurerm_resource_group.aks_rg.name
+}
+
+# Output AKS cluster name
+output "aks_cluster_name" {
+  value      = azurerm_kubernetes_cluster.aks_cluster.name
+  sensitive  = true  # Marking the cluster name as sensitive as indicated
 }
